c21e800eadcf00fc966f48c6d9d3ef52907d4a2f
kyw017763 <kyw017763@gmail.com>
2019-06-07 04:12:54 +0900

    first after create tables

--- dbconn.php
+++ dbconn.php
@@ -1,20 +1,34 @@
-<?php 
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("mysql://bed84a9b65a9ed:2c0e42a1@eu-cdbr-west-02.cleardb.net/heroku_e311a9b2e0b19bd?reconnect=true"));
+$cleardb_server   = $cleardb_url["u-cdbr-west-02.cleardb.net"];
+$cleardb_username = $cleardb_url["bed84a9b65a9ed"];
+$cleardb_password = $cleardb_url["2c0e42a1"];
+$cleardb_db       = substr($cleardb_url["heroku_e311a9b2e0b19bd?reconnect=true"],1);
 
-$hostname = "localhost";
-$username = "root";
-$password = "autoset";
-$dbname = "zteam";
 
-$conn = mysqli_connect($hostname, $username, $password, $dbname);
+$active_group = 'default';
+$query_builder = TRUE;
 
-/*
-if(!$conn){
-	echo "MySQL 접속 에러 : ";
-	echo mysqli_connect_error();
-	exit();
-} else {
-}
-*/
-
-mysqli_set_charset($conn, 'utf8');
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 ?>
