83b31fa506107030a867daade0e57576afeaf02e
kyw017763 <kyw017763@gmail.com>
2019-06-23 22:39:02 +0900

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,20 +1,12 @@
-<?php 
-
-$hostname = "localhost";
-$username = "root";
-$password = "autoset";
-$dbname = "zteam";
-
-$conn = mysqli_connect($hostname, $username, $password, $dbname);
-
-
-if(!$conn){
-	echo "MySQL 접속 에러 : ";
-	echo mysqli_connect_error();
-	exit();
-} else {
-}
-
-
+<?php
+$url = parse_url("mysql://bed84a9b65a9ed:2c0e42a1@eu-cdbr-west-02.cleardb.net/heroku_e311a9b2e0b19bd?reconnect=true");
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn = new mysqli($server, $username, $password, $db);
+if (!$conn) {
+    return die("Connection failed: " . mysqli_connect_error());
+};
 mysqli_set_charset($conn, 'utf8');
 ?>
